a378e24dc50c5c381af3c5b3d6c00086bbe0e709
cuddleofdeath <cuddleofdeath@gmail.com>
2022-05-17 21:41:56 -0500

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,22 +10,21 @@ function Navbar() {
 				{/* Logo */}
 				<div className='pt-2 text-white'>
 					<img src={Logo} alt='logo' />
+					<div className='text-white flex align-left'>ResortsOnChain</div>
 				</div>
+
 				{/* Menu items */}
 				<div className='hidden md:flex space-x-16 mt-2 text-white text-2xl'>
 					<a href='#'>Featured Stays</a>
-					<a href='#'>About Us</a>
 					<a href='#'>Get Listed</a>
-					<a href='#'>Contact</a>
+					<a href='#'>Roadmap</a>
 					<a href='#'>Partners</a>
-					<a href='#'>About</a>
+					<a href='#'>Contact</a>
+					<a href='#'>FAQ</a>
 				</div>
 				<div>
 					<Walletbtn />
 				</div>
-				{/* <p className="ml-2 text-3xl text-[#331684] font-['GeneralSans-Semibold']">
-					<img src={Logo} alt='logo' /> ResortsOnChain
-				</p> */}
 			</div>
 		</nav>
 	);
