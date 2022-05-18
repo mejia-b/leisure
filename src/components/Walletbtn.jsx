3d6521deb57e1f7318af8576cc47fb44ea71bf21
cuddleofdeath <cuddleofdeath@gmail.com>
2022-05-17 22:21:35 -0500

    Added hero

--- src/components/Walletbtn.jsx
+++ src/components/Walletbtn.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Walletbtn() {
 	return (
-		<button class='bg-[#8247e5] hover:bg-[#4A23A4] text-white font-regular py-4 px-8 shadow'>
+		<button class='bg-[#8247e5]  hover:bg-[#4A23A4] text-white font-bold py-4 px-8 shadow rounded-full'>
 			Connect Wallet
 		</button>
 	);
