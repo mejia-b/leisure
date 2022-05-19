f53bab9f9d549a042b58b01909684fbf93940c77
cuddleofdeath <cuddleofdeath@gmail.com>
2022-05-19 14:28:28 -0500

    added alternate navbar, exploring options

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar';
 import Walletbtn from './components/Walletbtn';
 import Hero from './components/Hero';
+import Nav from './components/Nav';
 function App() {
 	return (
 		<div className="bg-cover bg-center w-full h-screen bg-[url('./assets/images/background.png')]">
 			<Navbar />
+			{/* <Nav /> */}
 			{/* <div className='flex bg-white bg-opacity-30 shadow  py-5 px-8'></div> */}
 
 			<section id='hero'>
